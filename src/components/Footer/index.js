02645b7847b1f8a349db0df168c77cf960c804ee
Karen <karenyaa@live.co.uk>
2021-05-15 11:42:02 +0100

    added hero and row component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>© 2021 All Rights Reserved by Karen Opoku</span>
+    </footer>
+  );
+}
+
+export default Footer;
