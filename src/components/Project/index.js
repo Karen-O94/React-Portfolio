a39486ecbd1b2052fedb829508cc444dc7689987
Karen <karenyaa@live.co.uk>
2021-05-16 12:43:26 +0100

    added props to project component to pull information from project json

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,20 +2,20 @@ import React from "react";
 
 function Project(props) {
     return (
-        <div className="col-12 col-md-3 col-lg-3">
+        <div className="col-12 col-md-4 col-lg-4">
             <div className="card" style={{ width: "16rem" }}>
                 <h5 class="card-title">{props.name}</h5>
-                <img src={} class="card-img-top"
+                <img src={`${props.thumbnail}`} className="card-img-top"
                     alt="" />
                 <div className="card-body">
 
-                    <p className="card-text"> {props.description}</p>
+                    <p className="card-text"> {`${props.desc}`}</p>
                     <li>
-                        <a href={props.git_url} class="btn btn-dark">GitHub
+                        <a href={`${props.git_url}`} className="btn btn-dark">GitHub
                 Repo</a>
                     </li>
                     <li>
-                        <a href={props.app_url} class="btn btn-dark">Live
+                        <a href= {`${props.app_url}`} className="btn btn-dark">Live
                 App</a>
                     </li>
                 </div>
