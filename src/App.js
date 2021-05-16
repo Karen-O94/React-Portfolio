77aab092c9d0dae2e3d2c3ce890e079a7fb9658c
Karen <karenyaa@live.co.uk>
2021-05-16 14:55:50 +0100

    added background image to wrapper component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -13,10 +13,12 @@ function App() {
        <div>
         <Navbar />
         <Wrapper>
+          <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
+          </Switch>
         </Wrapper>
         <Footer />
       </div>
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./style.css";
+import imagebg from "../../img/image26669.jpg"
 
 function Wrapper(props) {
-  return <main className="wrapper">{props.children}</main>;
+  const bgimage = {
+    backgroundImage: `url(${imagebg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+};
+  return <main className="wrapper" style={bgimage}>{props.children}</main>;
 }
 
 export default Wrapper;
