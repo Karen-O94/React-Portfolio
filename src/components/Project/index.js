3f04f7fab5167025a5b073952de51f572effe80f
Karen <karenyaa@live.co.uk>
2021-05-16 14:09:23 +0100

    fixed issue with gifs not displaying on portfolio page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./style.css";
 
 function Project(props) {
     return (
-        <div className="col-12 col-md-4 col-lg-4">
-            <div className="card" style={{ width: "16rem" }}>
-                <h5 class="card-title">{props.name}</h5>
+        <div className="col-12 col-md-4 col-lg-4 ml-4 my-4">
+            <div className="card" style={{ backgroundColor:"#adb5bd", width: "18rem" }}>
+                <h5 className="card-title">{props.name}</h5>
                 <img src={`${props.thumbnail}`} className="card-img-top"
                     alt="" />
                 <div className="card-body">
